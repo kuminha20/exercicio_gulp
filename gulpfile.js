998b9f4cca9057f10e5da237a382cb9629a4284b
kuminha20 <davidkuminha@gmail.com>
2025-06-16 15:54:58 -0300

    Configuração inicial do projeto com Gulp

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+// Tarefa para compilar SASS
+function compileSass() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+}
+
+// Tarefa para comprimir imagens
+function compressImages() {
+    return gulp.src('images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+}
+
+// Tarefa para comprimir JavaScript
+function compressJS() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/js'));
+}
+
+// Tarefa padrão que executa todas as tarefas
+exports.default = gulp.series(compileSass, compressImages, compressJS);
+
+// Tarefas individuais
+exports.sass = compileSass;
+exports.images = compressImages;
+exports.js = compressJS; 
